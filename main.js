4fc847e170b2d14d97aa752a03583caf58a61931
Ryan Currie <ryan.currie@gmail.com>
2017-09-29 14:46:49 -0700

    Completed car movement. #6

--- main.js
+++ main.js
@@ -16,23 +16,23 @@ class Car {
   move() {
     switch (this.direction) {
       case 'n':
-        this.location[1] += this.speed
-        break
-      case 's':
         this.location[1] -= this.speed
         break
+      case 's':
+        this.location[1] += this.speed
+        break
       case 'e':
-        this.location[0] += this.speed
+        this.location[0] -= this.speed
         break
       case 'w':
-        this.location[0] -= this.speed
+        this.location[0] += this.speed
     }
   }
   static start(car) {
     car.timerId = setInterval(() => {
       car.move()
       console.log(car.location)
-    }, 200)
+    }, 16)
   }
   static stop(car) {
     if (car.timerId) {
@@ -47,6 +47,21 @@ function renderCar(car) {
   $car.setAttribute('id', 'player-car')
   $car.style.left = car.location[0] + 'px'
   $car.style.top = car.location[1] + 'px'
+
+  switch (car.direction) {
+    case 'e':
+      $car.style.transform = 'rotate(-90deg)'
+      break
+    case 'w':
+      $car.style.transform = 'rotate(90deg)'
+      break
+    case 'n':
+      $car.style.transform = 'rotate(0)'
+      break
+    case 's':
+      $car.style.transform = 'rotate(180deg)'
+      break
+  }
   return $car
 }
 
@@ -58,12 +73,9 @@ function renderTrack() {
 }
 
 const $game = document.querySelector('#game')
-const player = new Car('n', 20, [189.5, 1152])
+const player = new Car('n', 60, [189.5, 1152])
 $game.appendChild(renderCar(player))
 $game.appendChild(renderTrack())
-
-const $playerCar = document.querySelector('#player-car')
-const $carStyle = $playerCar.style
 
 let keyDown = false
 
@@ -86,19 +98,23 @@ window.addEventListener('keydown', (e) => {
   switch (e.keyCode) {
     case 37:
       player.turn('e')
-      $carStyle.transform = 'rotate(-90deg)'
       break
     case 39:
       player.turn('w')
-      $carStyle.transform = 'rotate(90deg)'
       break
     case 38:
       player.turn('n')
-      $carStyle.transform = 'rotate(0)'
       break
     case 40:
       player.turn('s')
-      $carStyle.transform = 'rotate(180deg)'
       break
   }
 })
+
+setInterval(function () {
+  $game.innerHTML = ''
+  const $track = renderTrack()
+  const $car = renderCar(player)
+  $game.appendChild($track)
+  $game.appendChild($car)
+}, 16)
