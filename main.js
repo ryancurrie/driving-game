766b79149c25106beec78591008fa5991ec8d8b3
Ryan Currie <ryan.currie@gmail.com>
2017-09-29 15:30:47 -0700

    Added boost feature. #8

--- main.js
+++ main.js
@@ -29,21 +29,40 @@ class Car {
     }
   }
   static start(car) {
-    car.timerId = setInterval(() => {
+    car.timerGo = setInterval(() => {
       car.move()
       console.log(car.location)
     }, 16)
   }
   static stop(car) {
-    if (car.timerId) {
-      clearInterval(car.timerId)
+    if (car.timerGo) {
+      clearInterval(car.timerGo)
+    }
+  }
+  static boost(car) {
+    car.timerBoost = setInterval(() => {
+      car.accelerate(1)
+      car.boosted = true
+      console.log(car.speed)
+    }, 16)
+  }
+  static noBoost(car) {
+    if (car.timerBoost) {
+      car.boosted = false
+      car.speed = 5
+      clearInterval(car.timerBoost)
     }
   }
 }
 
 function renderCar(car) {
   const $car = document.createElement('img')
-  $car.setAttribute('src', 'red-car.png')
+  if (car.boosted) {
+    $car.setAttribute('src', 'red-car.png')
+  }
+  else {
+    $car.setAttribute('src', 'blue-car.png')
+  }
   $car.setAttribute('id', 'player-car')
   $car.style.left = car.location[0] + 'px'
   $car.style.top = car.location[1] + 'px'
@@ -73,11 +92,12 @@ function renderTrack() {
 }
 
 const $game = document.querySelector('#game')
-const player = new Car('n', 60, [189.5, 1152])
+const player = new Car('n', 5, [79.5, 592])
 $game.appendChild(renderCar(player))
 $game.appendChild(renderTrack())
 
 let keyDown = false
+let boost = false
 
 window.addEventListener('keydown', (e) => {
   if (keyDown) return
@@ -108,6 +128,21 @@ window.addEventListener('keydown', (e) => {
     case 40:
       player.turn('s')
       break
+  }
+})
+
+window.addEventListener('keydown', (e) => {
+  if (boost) return
+  boost = true
+  if (e.keyCode === 66) {
+    Car.boost(player)
+  }
+})
+
+window.addEventListener('keyup', (e) => {
+  boost = false
+  if (e.keyCode === 66) {
+    Car.noBoost(player)
   }
 })
 
